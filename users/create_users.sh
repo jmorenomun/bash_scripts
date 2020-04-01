b8977f9099290b2f8b922911a5653139b6e6e446
jmorenomun <jmorenomun@uoc.edu>
2020-04-01 14:47:11 +0200

    update

--- users/create_users.sh
+++ users/create_users.sh
@@ -8,26 +8,24 @@ for user_data in $(echo "$list_users" | jq -c .[]); do
 	user_psw=$(echo $user_data | jq -r '.psw')
 	user_group=$(echo $user_data | jq -r '.group')
 
+	# Comprueba que existe el grupo o lo crea
+	grep -q "$user_group" /etc/group >/dev/null
+	if [ $? -ne 0 ];then
+		groupadd "$user_group"
+	fi
+
+	# Comprueba que no exista previamente el usuario
 	egrep -c "^$user_name" /etc/passwd >/dev/null
 	if [ $? -eq 0 ];then
 		echo "ERR: $user_name ya existe"
 		continue
 	else
-		useradd -m -p "$user_psw" "$user_name"
+		# Crea usuario asignando grupo primario
+		useradd -g "$user_group" -p "$user_psw" "$user_name"
 		if [ $? -eq 0 ];then
 			echo "$user_name creado correctamente"
-
-			grep -q "$user_group" /etc/group >/dev/null
-			if [ $? -ne 0 ];then
-				groupadd "$user_group"
-			fi
-
-			usermod -a -G "$user_group" "$user_name"
-			if [ $? -eq 0 ];then
-				echo "$user_name añadido correctamente a $user_group \n"
-			fi
-
+			echo $(groups "$user_name")
+			echo ""
 		fi
-
 	fi
 done
